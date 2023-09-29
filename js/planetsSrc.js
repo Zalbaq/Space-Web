4c289e86c2b3717f8589c1e9c58e5c287f54e408
Zalbaq <ihza.nk.160903@gmail.com>
2023-09-29 21:49:46 +0700

    create planetsSrc.js

---
+++ js/planetsSrc.js
@@ -0,0 +1,11 @@
+export const planetSrc = {
+  nebula: "../planets/nebula.png",
+  mercury: "../planets/mercury.png",
+  venus: "../planets/venus.png",
+  earth: "../planets/earth.png",
+  mars: "../planets/mars.png",
+  jupiter: "../planets/jupiter.png",
+  saturn: "../planets/saturn.png",
+  uranus: "../planets/uranus.png",
+  neptune: "../planets/neptune.png",
+};
